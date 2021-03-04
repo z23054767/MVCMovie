5246345d3499f7636e8bbb023f9cddb931c150ec
linliwei <z23054676@gmail.com>
2021-03-04 16:25:21 +0800

    main

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        // public string Welcome(string name, int numTimes = 1)
+        // {
+        //     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        // }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
