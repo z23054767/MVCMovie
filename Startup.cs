4abccdd39d79cc4ad886d898ed3fe8b9128cab6e
linliwei <z23054676@gmail.com>
2021-03-05 09:15:15 +0800

    main

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace MvcMovie
 {
     public class Startup
@@ -24,6 +27,10 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
